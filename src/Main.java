cae05130f5d35b5650a8a51eff0fdd4506cbf008
aerogizz <tylerjoseph.6@gmail.com>
2016-10-02 14:31:43 -0600

    Main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ *
+ *
+ * A simple paint program by Tyler Mattioli and Trevor Vannoy
+ * 
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+        Frame.getInstance();
+    }
+
+}
